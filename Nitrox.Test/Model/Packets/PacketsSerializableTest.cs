dbe0116c4fee305531393a68f7972ce52531cb8d
TwinBuilderOne <jackreynolds9128@gmail.com>
2022-09-14 16:27:53 -0500

    Deep initialization and comparison for unit tests

--- Nitrox.Test/Model/Packets/PacketsSerializableTest.cs
+++ Nitrox.Test/Model/Packets/PacketsSerializableTest.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
+using System.Reflection;
+using AutoBogus;
+using BinaryPack.Attributes;
+using KellermanSoftware.CompareNetObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nitrox.Test.Serialization.Helper;
 using NitroxModel.Packets;
 
 namespace NitroxTest.Model.Packets
@@ -10,32 +14,81 @@ namespace NitroxTest.Model.Packets
     [TestClass]
     public class PacketsSerializableTest
     {
+        static Assembly subnauticaModelAssembly;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            subnauticaModelAssembly = AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName("NitroxModel-Subnautica.dll"));
+        }
+
         [TestMethod]
         public void InitSerializerTest()
         {
-            // TODO: load NitroxModel-Subnautica assembly first
             Packet.InitSerializer();
         }
 
         [TestMethod]
         public void PacketSerializationTest()
         {
+            IEnumerable<Type> types = typeof(Packet).Assembly.GetTypes().Concat(subnauticaModelAssembly.GetTypes());
+            Dictionary<Type, Type[]> subtypesByBaseType = types
+                .Where(type => type.IsAbstract && !type.IsSealed && !type.ContainsGenericParameters && type != typeof(Packet))
+                .ToDictionary(type => type, type => types
+                    .Where(t => type.IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface).ToArray());
+
             Type testedType = null;
             List<Packet> packets = new();
 
-            foreach (Type type in typeof(Packet).Assembly.GetTypes()
-            .Where(p => typeof(Packet).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract))
+            foreach (Type type in types.Where(p => typeof(Packet).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract))
             {
                 testedType = type;
-                // TODO: Recursively initialize reference types (their serialization code will not be called if they're null)
-                packets.Add((Packet)FormatterServices.GetUninitializedObject(type));
+
+                object faker = typeof(NitroxAutoFaker<>).MakeGenericType(type)
+                    .GetConstructor(new Type[] { typeof(Dictionary<Type, Type[]>) })
+                    .Invoke(new object[] { subtypesByBaseType.ToDictionary(pair => pair.Key, pair => pair.Value) });
+
+                if (subtypesByBaseType.ContainsKey(type))
+                {
+                    for (int i = 0; i < subtypesByBaseType[type].Length; i++)
+                    {
+                        Packet packet = (Packet)typeof(AutoFaker<>).MakeGenericType(subtypesByBaseType[type][i])
+                        .GetMethod(nameof(AutoFaker<object>.Generate), new Type[] { typeof(string) })
+                        .Invoke(faker, new object[] { null });
+                        packets.Add(packet);
+                    }
+                }
+                else
+                {
+                    Packet packet = (Packet)typeof(AutoFaker<>).MakeGenericType(type)
+                    .GetMethod(nameof(AutoFaker<object>.Generate), new Type[] { typeof(string) })
+                    .Invoke(faker, new object[] { null });
+                    packets.Add(packet);
+                }
             }
+
+            Packet.InitSerializer();
+            bool failed = false;
+
+            CompareLogic logic = new();
+            logic.Config.SkipInvalidIndexers = true;
+            logic.Config.AttributesToIgnore.Add(typeof(IgnoredMemberAttribute));
 
             foreach (Packet packet in packets)
             {
                 testedType = packet.GetType();
                 Packet deserialized = Packet.Deserialize(packet.Serialize());
-                // TODO: Check for equality between packets
+
+                ComparisonResult result = logic.Compare(packet, deserialized);
+                if (!result.AreEqual)
+                {
+                    Console.WriteLine($"Differences found:\n{result.DifferencesString}");
+                }
+            }
+
+            if (failed)
+            {
+                Assert.Fail("Error: one or more differences were found.");
             }
         }
     }
---
+++ Nitrox.Test/Serialization/Helper/NitroxAutoFaker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AutoBogus;
+using NitroxModel.DataStructures.GameLogic;
+
+namespace Nitrox.Test.Serialization.Helper;
+
+public class NitroxAutoFaker<TType> : AutoFaker<TType> where TType : class
+{
+    public NitroxAutoFaker() : this(new Dictionary<Type, Type[]>()) { }
+
+    public NitroxAutoFaker(Dictionary<Type, Type[]> subtypesByBaseType)
+    {
+        Configure(newBinder =>
+        {
+            newBinder.WithBinder(new PacketAutoBinder(subtypesByBaseType))
+                     .WithOverride(new NitroxTechTypeOverride());
+
+            if (subtypesByBaseType.Values.Count != 0)
+            {
+                int highestAbstractObjectCount = subtypesByBaseType.Values.Max(objects => objects.Length);
+                newBinder.WithRepeatCount(Math.Max(2, highestAbstractObjectCount));
+            }
+        });
+        Binder = new PacketAutoBinder(subtypesByBaseType);
+    }
+
+    private class NitroxTechTypeOverride : AutoGeneratorOverride
+    {
+        public override bool CanOverride(AutoGenerateContext context)
+        {
+            return context.GenerateType == typeof(NitroxTechType);
+        }
+
+        public override void Generate(AutoGenerateOverrideContext context)
+        {
+            context.Instance = new NitroxTechType(context.Faker.PickRandom<TechType>().ToString());
+        }
+    }
+}
